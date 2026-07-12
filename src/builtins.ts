d6f440f8e64ead66c58421b67a37c4c7abf7877f
Stevan Little <stevan.little@gmail.com>
2026-07-12 13:59:34 +0200

    cleanup stuff

--- src/builtins.ts
+++ src/builtins.ts
@@ -1,9 +1,9 @@
 
 import {
-    type TERM, type LIST, type Env, type Builtin, type Num, type Bool, type ERROR,
-    isNil, isCons, isNum, isStr, isList,
+    type TERM, type LIST, type Env, type Builtin, type Num, type Str, type Bool, type ERROR,
+    isNil, isCons, isNum, isStr, isList, isLiteral,
     nil, cons, car, cdr, cadr, cddr, num, str, bool, sym, raise,
-    newEnv, bind, eq, list, pprint,
+    newEnv, bind, eq, list, pprint, uncons
 } from './terms.ts';
 
 // -----------------------------------------------------------------------------
@@ -66,17 +66,29 @@ export function liftNumBinOp (name : string, f : (n : number, m : number) => num
         if (isNum(n) && isNum(m)) {
             return num(f( n.value, m.value ));
         } else {
-            return raise(`Must be numbers, duh!`);
+            return raise(`TYPE-ERROR! - expected Nums got (${n.type} ${m.type})`);
         }
     })
 }
 
-export function liftNumBoolOp (name : string, f : (n : number, m : number) => boolean) : Builtin {
+export function liftStrBinOp (name : string, f : (n : string, m : string) => string) : Builtin {
+    return liftBinOp(name, (n : TERM, m : TERM) : Str | ERROR => {
+        if (isStr(n) && isStr(m)) {
+            return str(f( n.value, m.value ));
+        } else {
+            return raise(`TYPE-ERROR! - expected Strs got (${n.type} ${m.type})`);
+        }
+    })
+}
+
+type JSLiteral = number | string | boolean
+
+export function liftLiteralBoolOp (name : string, f : (n : JSLiteral, m : JSLiteral) => boolean) : Builtin {
     return liftBinOp(name, (n : TERM, m : TERM) : Bool | ERROR => {
-        if (isNum(n) && isNum(m)) {
+        if (isLiteral(n) && isLiteral(m)) {
             return bool(f( n.value, m.value ));
         } else {
-            return raise(`Must be numbers, duh!`);
+            return raise(`TYPE-ERROR! - expected Literals (Str, Num or Bool) got (${n.type} ${m.type})`);
         }
     })
 }
@@ -85,43 +97,47 @@ export function liftNumBoolOp (name : string, f : (n : number, m : number) => bo
 
 export function initalizeEnv (core : Env | undefined = undefined) : Env {
     let env : Env = core == undefined ? newEnv() : newEnv( core );
+
+    // numeric bin-ops
     env = bind( sym('+'), liftNumBinOp('+', (n, m) => n + m), env );
     env = bind( sym('-'), liftNumBinOp('-', (n, m) => n - m), env );
     env = bind( sym('*'), liftNumBinOp('*', (n, m) => n * m), env );
     env = bind( sym('/'), liftNumBinOp('/', (n, m) => n / m), env );
     env = bind( sym('%'), liftNumBinOp('%', (n, m) => n % m), env );
 
-    env = bind( sym('=='), liftNumBoolOp('==', (n, m) => n == m), env );
-    env = bind( sym('!='), liftNumBoolOp('!=', (n, m) => n != m), env );
-    env = bind( sym('<='), liftNumBoolOp('<=', (n, m) => n <= m), env );
-    env = bind( sym('<'),  liftNumBoolOp('<',  (n, m) => n <  m), env );
-    env = bind( sym('>='), liftNumBoolOp('>=', (n, m) => n >= m), env );
-    env = bind( sym('>'),  liftNumBoolOp('>',  (n, m) => n >  m), env );
+    // string bin-ops
+    env = bind( sym('~'), liftStrBinOp('~', (n, m) => n + m), env );
 
+    // literal comparison bin-ops
+    env = bind( sym('=='), liftLiteralBoolOp('==', (n, m) => n == m), env );
+    env = bind( sym('!='), liftLiteralBoolOp('!=', (n, m) => n != m), env );
+    env = bind( sym('<='), liftLiteralBoolOp('<=', (n, m) => n <= m), env );
+    env = bind( sym('<'),  liftLiteralBoolOp('<',  (n, m) => n <  m), env );
+    env = bind( sym('>='), liftLiteralBoolOp('>=', (n, m) => n >= m), env );
+    env = bind( sym('>'),  liftLiteralBoolOp('>',  (n, m) => n >  m), env );
+
+    // structural equality
     env = bind( sym('eq?'),  liftBinOp('eq?', (n, m) => bool(eq(n, m))),  env );
     env = bind( sym('ne?'),  liftBinOp('ne?', (n, m) => bool(!eq(n, m))), env );
 
-    env = bind( sym('nil?'), liftUnOp('nil?', (t)    => bool(isNil(t))),  env );
+    // type predicates
+    env = bind( sym('nil?'),  liftUnOp('nil?',  (t) => bool(isNil(t))),   env );
+    env = bind( sym('atom?'), liftUnOp('atom?', (t) => bool(!isList(t))), env );
 
+    // cons functions
+    env = bind( sym('cons'), liftBinOp('cons',  (h, t) => { if (isList(t))    return cons(h, t); return raise(`Expected a list for second arg to cons, not ${t.type}`); }), env );
+    env = bind( sym('car'),  liftUnOp('car',    (list) => { if (isCons(list)) return car(list);  return raise(`Expected a list for car, not ${list.type}`); }),  env );
+    env = bind( sym('cdr'),  liftUnOp('cdr',    (list) => { if (isCons(list)) return cdr(list);  return raise(`Expected a list for cdr, not ${list.type}`); }),  env );
+    env = bind( sym('cadr'), liftUnOp('cadr',   (list) => { if (isCons(list)) return cadr(list); return raise(`Expected a list for cadr, not ${list.type}`); }),  env );
+    env = bind( sym('cddr'), liftUnOp('cddr',   (list) => { if (isCons(list)) return cddr(list); return raise(`Expected a list for cddr, not ${list.type}`); }),  env );
+
+    // list functions
     env = bind( sym('list'), liftListOp('list', (args) => args), env );
-    env = bind( sym('head'), liftUnOp('head', (list) => {
-        if (isCons(list)) return car(list);
-        return raise(`Expected a list for head, not ${list.type}`);
-    }),  env );
-    env = bind( sym('tail'), liftUnOp('tail', (list) => {
-        if (isCons(list)) return cdr(list);
-        return raise(`Expected a list for tail, not ${list.type}`);
-    }),  env );
-    env = bind( sym('cons'), liftBinOp( 'cons', (h, t) => {
-        if (isList(t)) return cons(h, t);
-        return raise(`Expected a list for second arg to cons, not ${t.type}`);
-    }), env );
+    env = bind( sym('head'), liftUnOp('head',   (list) => { if (isCons(list)) return car(list); return raise(`Expected a list for head, not ${list.type}`); }),  env );
+    env = bind( sym('tail'), liftUnOp('tail',   (list) => { if (isCons(list)) return cdr(list); return raise(`Expected a list for tail, not ${list.type}`); }),  env );
 
-    env = bind( sym('pprint'), liftUnOp( 'pprint', (t) => {
-        console.log(pprint(t));
-        return nil;
-    }), env );
-
+    // utilities ...
+    env = bind( sym('pprint'),   liftListOp('pprint', (args) => { uncons(args).forEach((arg) => console.log(pprint(arg))); return nil; }), env );
     env = bind( sym('time'),     liftUnOp('time',     (t) => { if (!isStr(t)) return raise(`time expects a STR label, not ${t.type}`);     console.time(t.value);    return nil; }), env );
     env = bind( sym('time/end'), liftUnOp('time/end', (t) => { if (!isStr(t)) return raise(`time/end expects a STR label, not ${t.type}`); console.timeEnd(t.value); return nil; }), env );
 
